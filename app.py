b8aa5bf7d20f78fe3f3476af71c1468c446c75bd
Akil_mahbub <186370455+akilmahbub31@users.noreply.github.com>
2025-01-21 21:58:28 +0600

    hello world message

--- app.py
+++ app.py
@@ -1,18 +1,1 @@
-# flake8, black
-def sum():
-    a = 5
-    b = 10
-    return a + b
-
-
-sum()
-
-
-def add(a, b):
-    return a + b
-
-
-result = add(5 + 10)
-print(result)
-
-
+print("hello world")
